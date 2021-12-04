6ee16f2c2167962c2a30de5daa8a1b13272b0ab7
AdyanHarryPotterFan <95539829+AdyanHarryPotterFan@users.noreply.github.com>
2021-12-04 22:13:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function flipimage1(){
+    document.getElementById("img1").src="FlamboyantM.png";
+   }
+   
+   function flipimage2(){
+    document.getElementById("img2").src="CooperationM.png";
+   }
+
+   function flipimage3(){
+    document.getElementById("img3").src="ImproviseM.png";
+   }
+
+   function flipimage4(){
+    document.getElementById("img4").src="KaputM.png";
+   }
+
+   function flipimage5(){
+    document.getElementById("img5").src="ThesaurusM.png";
+   }
+
+   function flipimage6(){
+    document.getElementById("img6").src="XylographM.png";
+   }
